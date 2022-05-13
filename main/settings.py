acd8ffc57928ab2b86b4b0b1baa8734cd7edeb5c
needl3 <needle@arch-sama.localdomain>
2022-05-13 13:17:35 +0545

    CSRF trusted source updated for new domains

--- main/settings.py
+++ main/settings.py
@@ -137,6 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 CSRF_TRUSTED_ORIGINS = ['https://an1sh.herokuapp.com',
+                        'https://anishchapagai.com.np',
+                        'https://anischapagai.com.np',
+                        'https://an1sh.azurewebsites.net'
 ]
 
 # Mail Configurations
