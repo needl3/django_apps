fb37ec2d25102ffe991be60654698c65887cabee
needl3 <needle@arch-sama.localdomain>
2022-05-19 21:25:43 +0545

    Music sourced through database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-import uuid
+from django.core.files.storage import FileSystemStorage
+import uuid, os
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        self.delete(name)
+        return name
 
 # Create your models here.
 class Me(models.Model):
+    def get_available_name(self, name, max_length=None):
+        name = 'portfolio/static/portfolio/assets/music'
+        if os.path.exists(name):
+            os.remove(name)
+        return name
+
     name=models.CharField(max_length=100, default='Anish Chapagai')
 
     # This image link will contain 3 image links seperated by commas
     # Can't make it list here because default python list type is not supported
     # So will make list in views by splitting
     image_link = models.TextField(default="")
-    music = models.FileField(upload_to='portfolio/static/portfolio/assets/', default="")
+    music = models.FileField(upload_to=get_available_name, default="")
 
     def __str__(self):
         return self.name
