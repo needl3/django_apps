d8793412b6c669883a2627c685d99f3cf8f16883
needl3 <needle@arch-sama.localdomain>
2022-05-19 22:18:04 +0545

    Trying db pointed url instead of specifying directly

--- portfolio/views.py
+++ portfolio/views.py
@@ -15,6 +15,7 @@ def index(request):
         'Me':{
             'Name':Me.name,
             'Image':Me.image_link.split(','),
+            'Music':Me.music.url,
             },
         'Projects':[{
             'Name':i.name,
