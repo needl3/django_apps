29b5a56555b0980c94742e93f189591598875c52
needl3 <needle@arch-sama.localdomain>
2022-05-29 20:37:16 +0545

    revert env variable for debug state since not working in production

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ['dj_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
