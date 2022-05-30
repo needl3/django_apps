0a1ce4bb26b15281684942ee34e74024dcba2d51
needl3 <needle@arch-sama.localdomain>
2022-05-30 15:05:18 +0545

    Custom startup script for cronjobs configuration

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
-apt-get update -y
-apt-get install cron
-python manage.py crontab add
-service cron start
+#!/bin/sh
 
-python manage.py runserver 80
+# Copy all necessary steps defined from deployment
+cat /opt/startup/startup.sh | head -n -1 > startup.sh
+
+# Add your custom command here
+echo '
+apt-get update -y
+apt-get install cron -y
+python manage.py crontab add
+service cron start' >> startup.sh
+
+# Add last line that'll startup the server
+cat /opt/startup/startup.sh | tail -n 1 >> startup.sh
+
+chmod +x startup.sh
+
+./startup.sh
