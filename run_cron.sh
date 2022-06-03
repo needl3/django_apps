07ea48abfced9f99428b7422a56c93d2c56b3f6c
needl3 <needle@arch-sama.localdomain>
2022-06-03 22:36:57 +0545

    Using environment variable for timing by default

--- run_cron.sh
+++ run_cron.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+LOG_FILE=/home/needle/timer_update.log
 
-sleep_time=$((( 24 * 60 * 60 )))
+if [[ -z "${UPDATE_TIME}" ]];then
+	echo "No variable \$UPDATE_TIME defined. Using default 1 day timing" > $LOG_FILE
+	UPDATE_TIME=$((( 24 * 60 * 60 )))
+fi
 
 # $APP_PATH is exported as environment variable duirng deployment
 . $APP_PATH/antenv/bin/activate
@@ -10,9 +14,9 @@ do
 	# Fetch the job hash and apply timer
 	for i in $(python $APP_PATH/manage.py crontab show | awk 'NR>1 {print $1}');
 	do
-		echo "$(date): Commencing database update for $(python $APP_PATH/manage.py crontab show | grep $i)" > /home/timer_update.log
+		echo "$(date): Commencing database update for $(python $APP_PATH/manage.py crontab show | grep $i)" > $LOG_FILE
 		python $APP_PATH/manage.py crontab run $i
 	done
-	sleep $sleep_time
+	sleep $UPDATE_TIME
 done&
 disown
